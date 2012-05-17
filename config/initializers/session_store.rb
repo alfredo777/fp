8eeb72adbfc02376eb3f325077b39423ad3856b2
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-05-17 17:28:51 -0500

    commit generals

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Fp::Application.config.session_store :cookie_store, :key => '_fp_session',:secret => '15af2ebf75028709267b24bd6c3fb16ec7b864cd16bfc39265e2e47c9400896b6113bda3f427a91fde726ae8c1e4deaae6e62123bffd52837ee24da265aa0f2b'
+Fp::Application.config.session_store :cookie_store, :key => '_fp_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Fp::Application.config.session_store :active_record_store
+# Fp::Application.config.session_store :active_record_store
