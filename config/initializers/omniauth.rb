50e884bf0201309c3f6d32367b949855e992d5c1
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-05-23 01:46:46 -0500

    super duper token

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, '220846244674086', '9f4db46eda2de400b51eac00114ea044',:iframe => true, 
+  provider :facebook, '236652113026256', 'b5bb95a717cca28a8bad746ce9747985',:iframe => true, 
   
     :scope => 'email, user_about_me,user_activities,user_birthday,user_education_history,
                       user_events,user_groups,user_hometown,user_interests,user_likes, user_location,
