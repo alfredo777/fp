22a732767b00f5c8e24b882e8452701c334e79bc
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-05-23 01:56:41 -0500

    subiendo lento

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,9 @@ class SessionsController < ApplicationController
         def create
           omniauth = request.env["omniauth.auth"]
           user = User.find_by_provider_and_uid(omniauth["provider"], omniauth["uid"]) || User.create_with_omniauth(omniauth)
-          session[:user_id] = User.id
+          session[:user_id] = user.id
           redirect_to prayers_url, :notice => "Signed in!"
-
-
-    end
+        end
 
   def destroy
     session[:user_id] = nil
