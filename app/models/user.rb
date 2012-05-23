ac5bc516f76d7374d96a21a9dfcdf05bf8cedf9d
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-05-22 22:40:19 -0500

    modif create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@ class User < ActiveRecord::Base
    has_many :prayers
    has_many :sessions
    def self.create_with_omniauth(omniauth)
-
-    create! do |user|
        user.provider = omniauth["provider"]
        user.uid = omniauth["uid"]
        user.name = omniauth["info"]["name"]
        user.image = omniauth["info"]["image"]
        user.token = omniauth["credentials"]["token"]
-       user.save!
+      user.save
      end
+     
     def self.create_from_hash!(hash)
       create(:name => hash['info']['name'])
     end
